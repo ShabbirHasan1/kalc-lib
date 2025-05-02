49ad230558426c5ea54a72ef1079b00296b6c51e
thoth@damocles <jonathankurtz@protonmail.com>
2025-05-02 00:42:49 -0500

    implemented auto_set and From<bool> for Auto

--- src/units.rs
+++ src/units.rs
@@ -116,6 +116,25 @@ pub enum Auto {
     False,
     Auto,
 }
+
+impl From<bool> for Auto {
+    fn from(value: bool) -> Self {
+        if value {
+            Self::True
+        } else {
+            Self::False
+        }
+    }
+}
+
+impl Auto {
+    pub fn auto_set(&mut self, state: bool) {
+        if *self == Self::Auto {
+            *self = state.into()
+        }
+    }
+}
+
 #[derive(Default, Copy, Clone, PartialEq)]
 pub struct HowGraphing {
     pub graph: bool,
