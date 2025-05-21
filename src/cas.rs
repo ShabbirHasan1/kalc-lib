656dd38814747c9cc8e02870b6db15cba8b0f104
bgkillas <walhelm@proton.me>
2025-05-20 23:11:50 -0400

    improve code quality for cas significantly

--- src/cas.rs
+++ src/cas.rs
@@ -1,4 +1,4 @@
-use crate::complex::{NumStr, quartic};
+use crate::complex::{NumStr, cubic, quadratic, quartic};
 use crate::{
     complex::NumStr::{
         Comma, Division, Exponent, Func, LeftBracket, LeftCurlyBracket, Minus, Multiplication, Num,
@@ -8,6 +8,282 @@ use crate::{
     units::{Number, Options},
 };
 use rug::Complex;
+use std::ops::{AddAssign, DivAssign, MulAssign, SubAssign};
+#[derive(Clone)]
+struct Polynomial {
+    quotient: Vec<Complex>,
+    divisor: Vec<Complex>,
+}
+fn mul(mut lhs: Vec<Complex>, rhs: Complex) -> Vec<Complex> {
+    lhs.iter_mut().for_each(|lhs| *lhs *= rhs.clone());
+    lhs
+}
+fn mul_assign(lhs: &mut Vec<Complex>, rhs: &[Complex]) {
+    let v =
+        vec![Complex::new(rhs.first().map(|a| a.prec().0).unwrap_or(1)); rhs.len() + lhs.len() - 1];
+    let q = std::mem::replace(lhs, v);
+    for (j, b) in rhs.iter().enumerate() {
+        if b.is_zero() {
+            continue;
+        }
+        for (i, a) in q.iter().enumerate() {
+            if a.is_zero() {
+                continue;
+            }
+            lhs[i + j] += a * b.clone()
+        }
+    }
+}
+#[allow(clippy::suspicious_op_assign_impl)]
+impl SubAssign<Complex> for Polynomial {
+    fn sub_assign(&mut self, rhs: Complex) {
+        let prec = rhs.prec();
+        let divisor = mul(self.divisor.clone(), rhs);
+        self.quotient.extend(vec![
+            Complex::new(prec);
+            (divisor.len() + 1).saturating_sub(self.quotient.len())
+        ]);
+        self.quotient
+            .iter_mut()
+            .zip(divisor)
+            .for_each(|(a, b)| *a -= b);
+        self.simplify()
+    }
+}
+#[allow(clippy::suspicious_op_assign_impl)]
+impl SubAssign<Self> for Polynomial {
+    fn sub_assign(&mut self, mut rhs: Self) {
+        mul_assign(&mut self.quotient, rhs.divisor.as_slice());
+        mul_assign(&mut rhs.quotient, self.divisor.as_slice());
+        self.quotient.extend(vec![
+            Complex::new(self.divisor[0].prec());
+            (rhs.quotient.len() + 1)
+                .saturating_sub(self.quotient.len())
+        ]);
+        self.quotient
+            .iter_mut()
+            .zip(rhs.quotient)
+            .for_each(|(a, b)| *a -= b);
+        mul_assign(&mut self.divisor, rhs.divisor.as_slice());
+        self.simplify()
+    }
+}
+#[allow(clippy::suspicious_op_assign_impl)]
+impl AddAssign<Complex> for Polynomial {
+    fn add_assign(&mut self, rhs: Complex) {
+        let prec = rhs.prec();
+        let divisor = mul(self.divisor.clone(), rhs);
+        self.quotient.extend(vec![
+            Complex::new(prec);
+            (divisor.len() + 1).saturating_sub(self.quotient.len())
+        ]);
+        self.quotient
+            .iter_mut()
+            .zip(divisor)
+            .for_each(|(a, b)| *a += b);
+        self.simplify()
+    }
+}
+impl AddAssign<Self> for Polynomial {
+    fn add_assign(&mut self, mut rhs: Self) {
+        mul_assign(&mut self.quotient, rhs.divisor.as_slice());
+        mul_assign(&mut rhs.quotient, self.divisor.as_slice());
+        self.quotient.extend(vec![
+            Complex::new(self.divisor[0].prec());
+            (rhs.quotient.len() + 1)
+                .saturating_sub(self.quotient.len())
+        ]);
+        self.quotient
+            .iter_mut()
+            .zip(rhs.quotient)
+            .for_each(|(a, b)| *a += b);
+        mul_assign(&mut self.divisor, rhs.divisor.as_slice());
+        self.simplify()
+    }
+}
+impl DivAssign<Complex> for Polynomial {
+    fn div_assign(&mut self, rhs: Complex) {
+        self.divisor.iter_mut().for_each(|a| *a *= rhs.clone())
+    }
+}
+impl MulAssign<Complex> for Polynomial {
+    fn mul_assign(&mut self, rhs: Complex) {
+        self.quotient.iter_mut().for_each(|a| *a *= rhs.clone())
+    }
+}
+impl MulAssign<&Self> for Polynomial {
+    fn mul_assign(&mut self, rhs: &Self) {
+        mul_assign(&mut self.quotient, rhs.quotient.as_slice());
+        mul_assign(&mut self.divisor, rhs.divisor.as_slice());
+    }
+}
+#[allow(clippy::suspicious_op_assign_impl)]
+impl DivAssign<Self> for Polynomial {
+    fn div_assign(&mut self, rhs: Self) {
+        *self *= &rhs.recip()
+    }
+}
+impl From<Vec<Complex>> for Polynomial {
+    fn from(quotient: Vec<Complex>) -> Self {
+        let prec = quotient[0].prec();
+        let divisor = vec![Complex::with_val(prec, 1)];
+        Polynomial { quotient, divisor }
+    }
+}
+impl From<(Vec<Complex>, Vec<Complex>)> for Polynomial {
+    fn from(value: (Vec<Complex>, Vec<Complex>)) -> Self {
+        let (quotient, divisor) = value;
+        Polynomial { quotient, divisor }
+    }
+}
+impl Polynomial {
+    fn new(prec: u32) -> Self {
+        let divisor = vec![Complex::with_val(prec, 1)];
+        Self {
+            quotient: Vec::new(),
+            divisor,
+        }
+    }
+    fn recip(self) -> Self {
+        Polynomial {
+            quotient: self.divisor,
+            divisor: self.quotient,
+        }
+    }
+    fn simplify(&mut self) {
+        while self.quotient.last().map(|a| a.is_zero()).unwrap_or(false) {
+            self.quotient.pop();
+        }
+    }
+    fn compute(mut self) -> Result<Vec<Complex>, &'static str> {
+        fn last_non_zero(a: &[Complex]) -> Result<usize, &'static str> {
+            if a.is_empty() {
+                Err("zero polynomial")
+            } else {
+                Ok(a.len() - 1)
+            }
+        }
+        let d_div = last_non_zero(&self.divisor)?;
+        if d_div == 1 {
+            return Ok(self.quotient);
+        }
+        let lead_div = self.divisor[d_div].clone();
+        let mut d_rem = match last_non_zero(&self.quotient) {
+            Ok(d) => d,
+            Err(_) => return Ok(Vec::new()),
+        };
+        let mut quotient = vec![Complex::new(self.quotient[0].prec()); d_rem - d_div + 1];
+        while d_rem >= d_div {
+            let shift = d_rem - d_div;
+            let coeff = self.quotient[d_rem].clone() / lead_div.clone();
+            for k in 0..d_div {
+                self.quotient[k + shift] -= coeff.clone() * self.divisor[k].clone();
+            }
+            self.quotient.pop();
+            self.simplify();
+            quotient[shift] = coeff;
+            d_rem = match last_non_zero(&self.quotient) {
+                Ok(d) => d,
+                Err(_) => {
+                    return Ok(quotient);
+                }
+            };
+        }
+        Ok(quotient)
+    }
+    fn get_polynomial(
+        func: &[NumStr],
+        options: &Options,
+        var: String,
+    ) -> Result<Self, &'static str> {
+        if is_interior(func) {
+            return Self::get_polynomial(&func[1..func.len() - 1], options, var);
+        }
+        let mut arr = Polynomial::new(options.prec);
+        let list = place(func, &Plus, false);
+        let is_empty = list.is_empty();
+        for p in list {
+            poly_add(options, var.clone(), &mut arr, p)?;
+        }
+        if !is_empty {
+            return Ok(arr);
+        }
+        let list = place(func, &Minus, false);
+        let is_empty = list.is_empty();
+        for (k, p) in list.into_iter().enumerate() {
+            if k == 0 {
+                arr = Self::get_polynomial(p, options, var.clone())?;
+                continue;
+            }
+            if is_constant(p, var.clone()) {
+                arr -= do_math(p.to_vec(), *options, Vec::new())?.num()?.number
+            } else {
+                let q = Self::get_polynomial(p, options, var.clone())?;
+                arr -= q;
+            }
+        }
+        if !is_empty {
+            return Ok(arr);
+        }
+        let list = place(func, &Multiplication, false);
+        let is_empty = list.is_empty();
+        if !is_empty {
+            arr.quotient.push(Complex::with_val(options.prec, 1));
+        }
+        for p in list {
+            poly_mul(options, var.clone(), &mut arr, p)?;
+        }
+        if !is_empty {
+            return Ok(arr);
+        }
+        let list = place(func, &Division, false);
+        let is_empty = list.is_empty();
+        for (k, p) in list.into_iter().enumerate() {
+            if k == 0 {
+                arr = Self::get_polynomial(p, options, var.clone())?;
+                continue;
+            }
+            if is_constant(p, var.clone()) {
+                arr /= do_math(p.to_vec(), *options, Vec::new())?.num()?.number
+            } else {
+                let p = Self::get_polynomial(p, options, var.clone())?;
+                arr /= p;
+            }
+        }
+        if !is_empty {
+            return Ok(arr);
+        }
+        let mut list = place(func, &Exponent, true);
+        let is_empty = list.is_empty();
+        if !is_empty {
+            let p = list.remove(0);
+            let p = Self::get_polynomial(p, options, var.clone())?;
+            let k = do_math(list.remove(0).to_vec(), *options, Vec::new())?
+                .num()?
+                .number
+                .into_real_imag()
+                .0
+                .to_integer()
+                .unwrap_or_default();
+            arr = p.clone();
+            if k > 0 {
+                let mut i = rug::Integer::from(1);
+                while i < k {
+                    arr *= &p;
+                    i += 1;
+                }
+            }
+        }
+        if !is_empty {
+            return Ok(arr);
+        }
+        if func[0] == Func(var) {
+            arr.quotient.push(Complex::new(options.prec));
+            arr.quotient.push(Complex::with_val(options.prec, 1));
+        }
+        Ok(arr)
+    }
+}
 fn place<'a>(func: &'a [NumStr], target: &'a NumStr, once: bool) -> Vec<&'a [NumStr]> {
     let mut b = 0;
     let mut l = 0;
@@ -59,184 +335,31 @@ fn is_poly(func: &[NumStr], var: &str) -> bool {
         _ => false,
     })
 }
-fn poly_div(
-    mut remainder: [Complex; 5],
-    divisor: [Complex; 5],
-) -> Result<([Complex; 5], Option<[Complex; 5]>), &'static str> {
-    fn last_non_zero(a: &[Complex; 5]) -> Result<usize, &'static str> {
-        for i in (0..5).rev() {
-            if !a[i].is_zero() {
-                return Ok(i);
-            }
-        }
-        Err("zero polynomial")
-    }
-    let prec = remainder[0].prec();
-    let zero = || Complex::new(prec);
-    let mut quotient = [zero(), zero(), zero(), zero(), zero()];
-    let d_div = last_non_zero(&divisor)?;
-    let lead_div = divisor[d_div].clone();
-    let mut d_rem = match last_non_zero(&remainder) {
-        Ok(d) => d,
-        Err(_) => return Ok((quotient, None)),
-    };
-    while d_rem >= d_div {
-        let shift = d_rem - d_div;
-        let coeff = remainder[d_rem].clone() / lead_div.clone();
-        for k in 0..=d_div {
-            remainder[k + shift] -= coeff.clone() * divisor[k].clone();
-        }
-        quotient[shift] = coeff;
-        d_rem = match last_non_zero(&remainder) {
-            Ok(d) => d,
-            Err(_) => {
-                return Ok((quotient, None));
-            }
-        };
-    }
-    Ok((quotient, Some(remainder)))
-}
-fn get_polynomial(
-    func: &[NumStr],
-    options: &Options,
-    var: String,
-) -> Result<[Complex; 5], &'static str> {
-    if is_interior(func) {
-        return get_polynomial(&func[1..func.len() - 1], options, var);
-    }
-    let zero = || Complex::new(options.prec);
-    let mut arr = [zero(), zero(), zero(), zero(), zero()];
-    let list = place(func, &Plus, false);
-    let is_empty = list.is_empty();
-    for p in list {
-        poly_add(options, var.clone(), &mut arr, p)?;
-    }
-    if !is_empty {
-        return Ok(arr);
-    }
-    let list = place(func, &Minus, false);
-    let is_empty = list.is_empty();
-    for (k, p) in list.into_iter().enumerate() {
-        if k == 0 {
-            arr = get_polynomial(p, options, var.clone())?;
-            continue;
-        }
-        if is_constant(p, var.clone()) {
-            arr[0] -= do_math(p.to_vec(), *options, Vec::new())?.num()?.number
-        } else {
-            let q = get_polynomial(p, options, var.clone())?;
-            arr.iter_mut().zip(q.into_iter()).for_each(|(a, b)| *a -= b);
-        }
-    }
-    if !is_empty {
-        return Ok(arr);
-    }
-    let list = place(func, &Multiplication, false);
-    let is_empty = list.is_empty();
-    if !is_empty {
-        arr[0] += 1;
-    }
-    for p in list {
-        poly_mul(options, var.clone(), &mut arr, p)?;
-    }
-    if !is_empty {
-        return Ok(arr);
-    }
-    let list = place(func, &Division, false);
-    let is_empty = list.is_empty();
-    for (k, p) in list.into_iter().enumerate() {
-        if k == 0 {
-            arr = get_polynomial(p, options, var.clone())?;
-            continue;
-        }
-        if is_constant(p, var.clone()) {
-            for a in arr.iter_mut() {
-                *a /= do_math(p.to_vec(), *options, Vec::new())?.num()?.number
-            }
-        } else {
-            let p = get_polynomial(p, options, var.clone())?;
-            arr = poly_div(arr, p)?.0;
-        }
-    }
-    if !is_empty {
-        return Ok(arr);
-    }
-    let mut list = place(func, &Exponent, true);
-    let is_empty = list.is_empty();
-    if !is_empty {
-        let p = list.remove(0);
-        let p = get_polynomial(p, options, var.clone())?;
-        let k = do_math(list.remove(0).to_vec(), *options, Vec::new())?
-            .num()?
-            .number
-            .into_real_imag()
-            .0
-            .to_integer()
-            .unwrap_or_default();
-        arr = p.clone();
-        if k > 0 {
-            let mut i = rug::Integer::from(1);
-            while i < k {
-                poly_mult(&mut arr, &p)?;
-                i += 1;
-            }
-        }
-    }
-    if !is_empty {
-        return Ok(arr);
-    }
-    if func[0] == Func(var) {
-        arr[1] += 1
-    }
-    Ok(arr)
-}
-fn poly_mult(arr: &mut [Complex; 5], p: &[Complex; 5]) -> Result<(), &'static str> {
-    let prec = p[0].prec();
-    let zero = || Complex::new(prec);
-    let q = std::mem::replace(arr, [zero(), zero(), zero(), zero(), zero()]);
-    for (j, b) in p.iter().enumerate() {
-        if b.is_zero() {
-            continue;
-        }
-        for (i, a) in q.iter().enumerate() {
-            if a.is_zero() {
-                continue;
-            }
-            if i + j > 4 {
-                return Err("poly too high");
-            }
-            arr[i + j] += a * b.clone()
-        }
-    }
-    Ok(())
-}
 fn poly_mul(
     options: &Options,
     var: String,
-    arr: &mut [Complex; 5],
+    arr: &mut Polynomial,
     p: &[NumStr],
 ) -> Result<(), &'static str> {
     if is_constant(p, var.clone()) {
-        for a in arr.iter_mut() {
-            *a *= do_math(p.to_vec(), *options, Vec::new())?.num()?.number
-        }
+        *arr *= do_math(p.to_vec(), *options, Vec::new())?.num()?.number
     } else {
-        let p = get_polynomial(p, options, var.clone())?;
-        poly_mult(arr, &p)?
+        let p = Polynomial::get_polynomial(p, options, var.clone())?;
+        *arr *= &p
     }
     Ok(())
 }
 fn poly_add(
     options: &Options,
     var: String,
-    arr: &mut [Complex; 5],
+    arr: &mut Polynomial,
     p: &[NumStr],
 ) -> Result<(), &'static str> {
     if is_constant(p, var.clone()) {
-        arr[0] += do_math(p.to_vec(), *options, Vec::new())?.num()?.number
+        *arr += do_math(p.to_vec(), *options, Vec::new())?.num()?.number
     } else {
-        let q = get_polynomial(p, options, var.clone())?;
-        arr.iter_mut().zip(q.iter()).for_each(|(a, b)| *a += b);
+        let q = Polynomial::get_polynomial(p, options, var.clone())?;
+        *arr += q
     }
     Ok(())
 }
@@ -249,8 +372,43 @@ fn isolate_inner(
         return isolate_inner(&func[1..func.len() - 1], options, var);
     }
     if is_poly(func, &var) {
-        let [e, d, c, b, a] = get_polynomial(func, options, var)?.map(|a| Number::from(a, None));
-        let r = quartic(a, b, c, d, e, false);
+        let p: Vec<Complex> = Polynomial::get_polynomial(func, options, var)?.compute()?;
+        let l = p.len();
+        let mut p = p.into_iter();
+        let n = |c: Complex| Number::from(c, None);
+        let r = match l {
+            1 => vec![Number::from(
+                Complex::with_val(options.prec, rug::float::Special::Nan),
+                None,
+            )],
+            2 => {
+                let a = p.next().unwrap();
+                let b = p.next().unwrap();
+                vec![Number::from(-a / b, None)]
+            }
+            3 => {
+                let c = p.next().unwrap();
+                let b = p.next().unwrap();
+                let a = p.next().unwrap();
+                quadratic(n(a), n(b), n(c), false)
+            }
+            4 => {
+                let d = p.next().unwrap();
+                let c = p.next().unwrap();
+                let b = p.next().unwrap();
+                let a = p.next().unwrap();
+                cubic(n(a), n(b), n(c), n(d), false)
+            }
+            5 => {
+                let e = p.next().unwrap();
+                let d = p.next().unwrap();
+                let c = p.next().unwrap();
+                let b = p.next().unwrap();
+                let a = p.next().unwrap();
+                quartic(n(a), n(b), n(c), n(d), n(e), false)
+            }
+            _ => return Err("poly greater then quartic"),
+        };
         let mut v = Vec::new();
         v.push(LeftCurlyBracket);
         for o in r.into_iter().map(|a| Num(Box::new(a))) {
